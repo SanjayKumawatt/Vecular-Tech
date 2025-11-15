d73dd9b012131aef8f27c5dfab8136d2cdaf1db9
Sanjay Kumawat <sk9785699310@gmail.com>
2025-11-15 22:05:09 +0530

    message

--- src/Layout/AppLayout.jsx
+++ src/Layout/AppLayout.jsx
@@ -3,10 +3,12 @@ import Navbar from '../Components/Header'
 import { Home } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Components/Footer'
+import ScrollToTop from '../Components/ScrollToTop'
 
 const AppLayout = () => {
   return (
     <div>
+      <ScrollToTop/>
         <Navbar/>
         <Outlet/>
         <Footer/>
